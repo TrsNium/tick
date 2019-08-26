c54e73981eccc82dbcb86239c29e1ab166da7c34
TrsNium <trsnium@gmail.com>
2019-08-26 23:06:11 +0900

    add application

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule Tick.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :cowboy, :plug]
+      extra_applications: [:logger, :cowboy, :plug],
+      mod: {Tick.Application, []}
     ]
   end
 
